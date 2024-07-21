01b8ce7002e65081550e2eb05d076d9b548866b3
nayanajmera <nayanajmera1234@gmail.com>
2024-07-22 05:20:33 +0530

    Hard Care Changes

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 let btn=document.querySelector("button");
-let speed=document.getElementById("speed");
-// let sizeImg1=237640;
-// let sizeImg2=1917448;
-let sizeImg4=8224768;
+let speed=document.getElementById("num-speed");
+let avgSpeed=document.getElementById("avgSpeed");
+// slider code
+let btnSlider=document.querySelector(".slider-container");
+let slider=document.getElementById("slider");
+let body=document.querySelector("body");
+let img=document.querySelector("img");
+let h1=document.querySelector("h1");
+let lightMode=document.getElementById("light-m");
+let darkMode=document.getElementById("dark-m");
+lightMode.classList.add("invisible");
+//slider code
+let allSpeeds=[];
+//SIZES ARE IN BITS
+let sizeImg1=32768*8; //32KB
+let sizeImg2=159744*8;//156KB
+let sizeImg4=8224768;//0.98mb or 1003.52KB
 // let sizeImg5=66890744;
-// let url1="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Strigeria_Centipede_guarding_eggs.jpg/330px-Strigeria_Centipede_guarding_eggs.jpg?";
-// let url2="https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png?";
+let url1="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Strigeria_Centipede_guarding_eggs.jpg/330px-Strigeria_Centipede_guarding_eggs.jpg?";
+let url2="https://upload.wikimedia.org/wikipedia/commons/f/f8/Chinta_Wrong_Direction.jpg?";
 // let url4="https://upload.wikimedia.org/wikipedia/commons/5/53/Panorama_of_3mb.jpg?";
 let url4="https://upload.wikimedia.org/wikipedia/commons/7/7a/2017-05-23_GuentherZ_Wien11_Zentralfriedhof_Gruppe97_Soldatenfriedhof_Wien_%28Zweiter_Weltkrieg%29_%28001%29.jpg?";
 // let url5="https://upload.wikimedia.org/wikipedia/commons/9/90/Teruel_overview_size_8MB_%2825441803782%29.jpg?";
@@ -35,9 +48,14 @@ async function getInternetSpeed(){
         time=1;
     }
     time=time/1000;
+    time=90*time/100;//considering delay from urls
+    if(currentURL!=url1){
+        time=80*time/100;
+    }
     console.log(currentSize, time);
     let speed_bps = currentSize/time;//size of Image is in bits
     let speed_kbps= speed_bps/1024;//in kilobits per second
+    //If I am using URL4 then I am assuming that the speed is above 500KBPS or 4000Kbps 
     // if(time<0.9){
     //     currentURL=url2;//bigger url //around 200kb
     //     currentSize=sizeImg2;
@@ -62,23 +80,42 @@ async function getInternetSpeed(){
     //     currentURL=url3;
     //     currentSize=sizeImg3;
     // }
-    // if(time>1.5){
-    //     currentURL=url2;
-    //     currentSize=sizeImg2;
-    // }
+    if(speed_kbps<1500){
+        currentURL=url1;
+        currentSize=sizeImg1;
+    }
+    else if(speed_kbps<5000){
+        currentURL=url2;
+        currentSize=sizeImg2;
+    }
+    else if(speed_kbps>7500){
+        currentURL=url4;
+        currentSize=sizeImg4;
+    }
+    //I am still assumiing that user should have a speed of at least 115KBps
     return speed_kbps;
 }
-async function mentionSpeed(){
-    let sum=0;
-    for(let i=1; i<=3; i++){
-        let intSpeed=await getInternetSpeed();
-        sum+=intSpeed;
-    }
-    let avgSpeed=sum/3; 
-    speed.innerText=`${(avgSpeed/1024).toFixed(2)} Mbps`;
+async function mentionSpeed()
+{
+        let sum=0;
+        for(let i=1; i<=3; i++){
+            let intSpeed= await getInternetSpeed();
+            sum+=intSpeed;
+        }
+        let avgSpeed=sum/3;
+        allSpeeds.push(avgSpeed);
+        speed.innerText=`${(avgSpeed/1024).toFixed(2)}`;
 }
 function changeColor(clr){
     btn.style.color=clr;
+}
+function calculateAvgOfAll(){
+    let AvgOfAll=0;
+    for(let i=0; i<allSpeeds.length; i++){
+        AvgOfAll+=allSpeeds[i];
+    }
+    AvgOfAll=AvgOfAll/allSpeeds.length;
+    avgSpeed.innerText=`Average Speed = ${(AvgOfAll/1024).toFixed(2)} Mbps`;
 }
 let intervalId=null;
 let alreadyclicked=false;
@@ -86,31 +123,80 @@ let timeout=null;
 btn.addEventListener("click", async function() {
         if(alreadyclicked==false)
         {
+            allSpeeds=[];
+            avgSpeed.innerText="";
             alreadyclicked=true;
             changeColor(`red`);
             btn.innerText=`Stop`;
             clearInterval(intervalId);
-            intervalId = setInterval(mentionSpeed, 5000);
+            intervalId = setInterval(mentionSpeed, 3000);
             if(alreadyclicked==true)
             {
                 timeout=setTimeout(() => {
                     clearInterval(intervalId);
-                    changeColor(`darkblue`);
+                    calculateAvgOfAll();
+                    changeColor(`rgb(0,200,0)`);
                     btn.innerText=`Check Speed`;
                     alreadyclicked=false;
-                }, 60000);
+                }, 30000);
             }
         }
         else{
             alreadyclicked=false;
             clearTimeout(timeout);
             clearInterval(intervalId);
-            changeColor(`darkblue`);
+            changeColor(`rgb(0,200,0)`);
             btn.innerText=`Check Speed`;
+            calculateAvgOfAll();
         }
 });
-
-
+function darkmode(){
+    slider.classList.add("active");
+    btnSlider.classList.add("white");
+    body.style.backgroundColor="#2b2b2b";
+    img.setAttribute("src", "./OIG2-removebg-preview-modified.png");
+    h1.style.color="white";
+    btn.classList.add("dark-mode");
+    lightMode.classList.remove("invisible");
+    darkMode.classList.add("invisible");
+    lightMode.style.color="white";
+    darkMode.style.color="white";
+}
+function lightmode(){
+    slider.classList.remove("active");
+    btnSlider.classList.remove("white");
+    body.style.backgroundColor="white";
+    img.setAttribute("src", "./OIG2-removebg-preview.png");
+    h1.style.color="black";
+    btn.classList.remove("dark-mode");
+    darkMode.classList.remove("invisible");
+    lightMode.classList.add("invisible");
+    lightMode.style.color="black";
+    darkMode.style.color="black";
+}
+btnSlider.addEventListener("click", function(){
+    if(slider.classList.length==0)//dark mode
+    {
+        darkmode();
+    }
+    else{
+        lightmode();
+    }
+});
+// Handling icon stuff:-
+function updateFaviconChangeMode(){
+    let isDarkMode=window.matchMedia('(prefers-color-scheme: dark)').matches;
+    let favicon=document.getElementById("favicon");
+    favicon.href= isDarkMode ? "./OIG2-removebg-preview-modified.png" : "./OIG2-removebg-preview.png";
+    if(isDarkMode){
+        darkmode();
+    }
+    else{
+        lightmode();
+    }
+}
+updateFaviconChangeMode();
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", updateFaviconChangeMode);
 //FALTU STUFF JUST FOR CHECKING CONCEPTS
 // async function timepass(){
 //    return new Promise((resolve, reject)=>{
